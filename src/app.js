9aea0a7b4c8b3d88d6ebfbfae1aaa1205c13e6a5
anvitad-sdei <dixitanvita446@gmail.com>
2026-03-04 00:02:25 +0530

    api implemented

--- src/app.js
+++ src/app.js
@@ -1,76 +1,137 @@
 //starting point of application
 
 const express = require("express");
-
+const { adminAuth } = require("./middlewares/auth");
+const { connectDB } = require("./config/database.js");
+require("./config/database.js");
 const app = express();
+const User = require("./model/user");
 
 // if we will create url like thie ab?c then in this case "b" is optional if wew pass in url /ac or /abc in both scenerio it will work
 // ab+c then "b" can add as many of times like "abbbc" will work
 // ab*cd this means you can add any text between ab and cd but pattern should be match starting with ab and end with cd.
 // a(bc)?d in this case "bc" is optional
 // /a/ in url a letter exits then url will work
-app.get("/abc", (req, res) => {
-  res.send({ firstName: "Krishnavi", lastName: "Dixit" });
-});
+// app.get("/abc", (req, res) => {
+//   res.send({ firstName: "Krishnavi", lastName: "Dixit" });
+// });
 
-//get() method to match only GET method api call to 'user'
-app.get("/user", (req, res) => {
-  console.log(req.query); // req.query will print the value pass the in the url like ?id
-  res.send({ firstName: "Krishnavi", lastName: "Dixit" });
-});
+// //get() method to match only GET method api call to 'user'
+// app.get("/user", (req, res) => {
+//   console.log(req.query); // req.query will print the value pass the in the url like ?id
+//   res.send({ firstName: "Krishnavi", lastName: "Dixit" });
+// });
 
 // :(colons) means dynamic routes
-app.get("/user/:userId", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Krishnavi", lastName: "Dixit" });
-});
+// app.get("/user/:userId", (req, res) => {
+//   console.log(req.params);
+//   res.send({ firstName: "Krishnavi", lastName: "Dixit" });
+// });
 
-app.post("/user", (req, res) => {
-  //Store the data into DB
-  res.send("Successfully saved data!");
-});
+// app.post("/user", (req, res) => {
+//   //Store the data into DB
+//   res.send("Successfully saved data!");
+// });
 
-app.delete("/user", (req, res) => {
-  res.send("delete data");
-});
+// app.delete("/user", (req, res) => {
+//   res.send("delete data");
+// });
 
 // use() method will match the all HTTP method API calls
-app.use("/hello", (req, res) => {
-  res.send("Hello hello hello from the server radha rani!");
-});
+// app.use("/hello", (req, res) => {
+//   res.send("Hello hello hello from the server radha rani!");
+// });
 
-app.use("/user1", (req, res) => {
-  //If you are not sending any response from the server then after sometime timeout will happen
-});
+// app.use("/user1", (req, res) => {
+//   //If you are not sending any response from the server then after sometime timeout will happen
+// });
 
 // we can call multiple route handler and for next route handler we have one another parameter next to pass
-app.use(
-  "/user2",
-  (req, res, next) => {
-    // res.send("response1");
-    console.log("response 1!");
-    next();
-  },
-  [
-    (req, res, next) => {
-      console.log("response 2!");
-      next();
-    },
-    (req, res, next) => {
-      console.log("response 3!");
-      next();
-    },
-  ],
-  (req, res, next) => {
-    console.log("response 4!");
-    // res.send("response4");
-    next();
-  },
-  (req, res, next) => {
-    console.log("response 5!");
-    res.send("response5");
-  },
-);
-app.listen(3000, () => {
-  console.log("server is successfully listening on port 3000");
+// app.use(
+//   "/user2",
+//   (req, res, next) => {
+//     // res.send("response1");
+//     console.log("response 1!");
+//     next();
+//   },
+//   [
+//     (req, res, next) => {
+//       console.log("response 2!");
+//       next();
+//     },
+//     (req, res, next) => {
+//       console.log("response 3!");
+//       next();
+//     },
+//   ],
+//   (req, res, next) => {
+//     console.log("response 4!");
+//     // res.send("response4");
+//     next();
+//   },
+//   (req, res, next) => {
+//     console.log("response 5!");
+//     res.send("response5");
+//   },
+// );
+
+// what is middleware and why do we need
+// middleware are the multiple route handler defined inside the function
+
+// app.use("/admin", adminAuth);
+
+// app.get("/admin/getAllData", (req, res) => {
+//   res.send("all data sent");
+// });
+
+app.use(express.json());
+
+//Signup API
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("saved successfully!");
+  } catch (err) {
+    res.status(400).send("error saving the user:" + err.message);
+  }
 });
+
+//Get  users by emailId
+
+app.get("/user", async (req, res) => {
+  const userEmailId = req.body.emailId;
+  console.log(req, "req");
+  try {
+    const users = await User.find({ emailId: userEmailId });
+    if (users.length !== 0) {
+      res.send(users);
+    } else {
+      res.status(404).send("Not found");
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//get all users
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+connectDB()
+  .then(() => {
+    console.log("connected successfully");
+    app.listen(3000, () => {
+      console.log("server os runing");
+    });
+  })
+  .catch((err) => {
+    console.log("error coonection");
+  });
